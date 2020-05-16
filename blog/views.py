791e54091cef60067cb9c2ecec32b900898c5109
Raych118 <rachelmakwara@gmail.com>
2020-05-16 02:54:33 +0200

    Added site CSS

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
+
